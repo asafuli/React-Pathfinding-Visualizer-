dbfc41a69433d0e0677b8c917b03fe0fad5cfa37
Asaf Eli <ASSAFEL@amdocs.com>
2021-10-31 21:41:31 +0200

    styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,35 +14,43 @@ function Navbar({handleVisualizeClick, handleAddMaze, handleClrMaze,handleSetSpe
         <div className='alogrithms-list-header'>Algorithms <i className="fas fa-caret-down" onClick={() => toggleAlgoDropdown()}></i></div>
         <ul className={`algorithms-list ${AlgoDropdownOpened ? 'opened' : ''}`}>
           <li className="algorithm-list-item"> 
-          <button className="visualizer-btn" onClick={() => handleVisualizeClick('A*')}>
+          <button className="visualizer-btn" onClick={() => {toggleAlgoDropdown(); handleVisualizeClick('A*')}}>
          A*
         </button>
           </li>
           <li className="algorithm-list-item"> 
-          <button className="visualizer-btn" onClick={() => handleVisualizeClick('dijkstra')}>
+          <button className="visualizer-btn" onClick={() => {toggleAlgoDropdown();
+            handleVisualizeClick('dijkstra')}}>
          Dijkstra
         </button></li>
           <li className="algorithm-list-item"> 
-          <button className="visualizer-btn" disabled onClick={() => handleVisualizeClick('dijkstra')}>
-        Visualize BFS
+          <button className="visualizer-btn" disabled onClick={() => {toggleAlgoDropdown();
+          handleVisualizeClick('BFS')}}>
+         BFS
         </button>
            </li>
           <li className="algorithm-list-item">
-          <button className="visualizer-btn" disabled onClick={() => handleVisualizeClick('dijkstra')}>
-        Visualize DFS
+          <button className="visualizer-btn" disabled onClick={() => {toggleAlgoDropdown();handleVisualizeClick('DFS')}}>
+         DFS
         </button>
              </li>
         </ul>
       </li>
       <li className="navbar-item">
         <button className="add-maze-btn" onClick={() => handleAddMaze()}>Add Maze</button>
-        <button className="clr-maze-btn" onClick={() => handleClrMaze()}>New Board</button>
+      </li>
+      <li className="navbar-item">
+        <button className="new-board-btn" onClick={() => handleClrMaze()}>New Board</button>
+      </li>
+      <li className="navbar-item "> 
+          <button className="visualizer-btn visualize-btn" onClick={() => {toggleAlgoDropdown(); handleVisualizeClick('A*')}}>
+         Visualize
+        </button>
       </li>
       <li className="navbar-item" id="animation-speed">
         <span>Set Animation Speed </span>
         <button className="set-speed-btn" onClick={() => handleSetSpeed(1)}>+</button>
         <button className="set-speed-btn" onClick={() => handleSetSpeed(-1)}>-</button>
-        
       </li>
     </ul>
   )
