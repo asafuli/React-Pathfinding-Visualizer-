21433e304be58b86414e504a9b21006bb1be7b84
Asaf Eli <ASSAFEL@amdocs.com>
2021-10-31 15:04:28 +0200

    dropdown for algo first revision

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,42 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 function Navbar({handleVisualizeClick, handleAddMaze, handleClrMaze,handleSetSpeed, createNewBoard}) {
-  
+
+  const [AlgoDropdownOpened, setAlgoDropdownOpened] = useState(false);
+
+  const toggleAlgoDropdown = () => {
+    setAlgoDropdownOpened(AlgoDropdownOpened => !AlgoDropdownOpened);
+  }
+
   return (
     <ul className="navbar-list">
       <li className="navbar-item">
-        Algorithms (A*) 
+        <div className='alogrithms-list-header'>Algorithms <i className="fas fa-caret-down" onClick={() => toggleAlgoDropdown()}></i></div>
+        <ul className={`algorithms-list ${AlgoDropdownOpened ? 'opened' : ''}`}>
+          <li className="algorithm-list-item"> 
+          <button className="visualizer-btn" onClick={() => handleVisualizeClick('A*')}>
+         A*
+        </button>
+          </li>
+          <li className="algorithm-list-item"> 
+          <button className="visualizer-btn" onClick={() => handleVisualizeClick('dijkstra')}>
+         Dijkstra
+        </button></li>
+          <li className="algorithm-list-item"> 
+          <button className="visualizer-btn" disabled onClick={() => handleVisualizeClick('dijkstra')}>
+        Visualize BFS
+        </button>
+           </li>
+          <li className="algorithm-list-item">
+          <button className="visualizer-btn" disabled onClick={() => handleVisualizeClick('dijkstra')}>
+        Visualize DFS
+        </button>
+             </li>
+        </ul>
       </li>
       <li className="navbar-item">
         <button className="add-maze-btn" onClick={() => handleAddMaze()}>Add Maze</button>
         <button className="clr-maze-btn" onClick={() => handleClrMaze()}>New Board</button>
-      </li>
-      <li className="navbar-item">
-        <button className="visualizer-btn" onClick={() => handleVisualizeClick('A*')}>
-        Visualize A*
-        </button>
-        <button className="visualizer-btn" onClick={() => handleVisualizeClick('dijkstra')}>
-        Visualize Dijkstra
-        </button>
-        <button className="visualizer-btn" disabled onClick={() => handleVisualizeClick('dijkstra')}>
-        Visualize BFS
-        </button>
-        <button className="visualizer-btn" disabled onClick={() => handleVisualizeClick('dijkstra')}>
-        Visualize DFS
-        </button>
-        
       </li>
       <li className="navbar-item" id="animation-speed">
         <span>Set Animation Speed </span>
